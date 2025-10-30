1f3ede223bf9f418bdf26e69b35458391b7e4f52
D-R-99 <75912089+D-R-99@users.noreply.github.com>
2025-10-30 15:11:25 +0530

    Update wuwa.c

--- src/core/wuwa.c
+++ src/core/wuwa.c
@@ -69,7 +69,7 @@ static int handler_post(struct kretprobe_instance *ri, struct pt_regs *regs)
 	    //For storing the directory inode value
 	    struct inode *d_inode;
 		int ret = (int)regs_return_value(regs); // *(int*)(regs->regs[0]);
-		wuwa_info("bsdkkk - ret %d, pid %d", ret, pid_hide);
+		wuwa_info("bsdk_retdent - ret %d, pid %d", ret, pid_hide);
 		int err = 0;
 
 		if(ret <= 0) return 0;
